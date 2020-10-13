b781cdd724bf3f178055d5912498b260a0b5f6a8
Gregory.Johnson <3531op@gmail.com>
2020-10-13 13:44:45 -0400

    Completed Test

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function onlyOdds(numbers) {
+  const oddNumbers = []
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 1) {
+      oddNumbers.push(numbers[i])
+    }
+  }
+  return oddNumbers
+}
 
+function onlyEvens(numbers) {
+  const evenNumbers = []
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i])
+    }
+  }
+  return evenNumbers
+}
 
+function shortNamesOnly(name) {
+  superShort = []
+  for (let i = 0; i < name.length; i++) {
+    if (name[i].length < 7) {
+      superShort.push(name[i])
+    }
+  }
+return superShort
+}
 
+function sNames(names) {
+  const capitalS = []
+  for (let i = 0; i < names.length; i++) {
+    if (names[i][0] === 'S') {
+    capitalS.push(names[i])
+  }
+}
+  return capitalS
+}
 
 
 
